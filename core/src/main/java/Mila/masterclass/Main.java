73c5968a5306df4d269fc997e2921005d33d4300
Mila Rose <milarose.dev@gmail.com>
2024-01-31 00:56:36 +0000

    First commit package name needs updating

---
+++ core/src/main/java/Mila/masterclass/Main.java
@@ -0,0 +1,15 @@
+package Mila.masterclass;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Main class is running Test");
+
+    }
+}
